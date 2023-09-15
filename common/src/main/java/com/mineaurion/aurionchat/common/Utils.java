bc551a521b90abd81b69cce5894102c2de06099e
Yann <yann24.bonnau@hotmail.fr>
2023-09-15 16:55:23 +0200

    fix: remove style when no permission to use it

--- common/src/main/java/com/mineaurion/aurionchat/common/Utils.java
+++ common/src/main/java/com/mineaurion/aurionchat/common/Utils.java
@@ -1,14 +1,28 @@
 package com.mineaurion.aurionchat.common;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import static net.kyori.adventure.text.format.NamedTextColor.WHITE;
 
 public class Utils {
     public static Component processMessage(String format, Component message, AurionChatPlayer aurionChatPlayer){
         if(!aurionChatPlayer.isAllowedColors()){
-            message = message.color(WHITE);
+            Component messageWithoutStyle = Component.text("");
+            if(!message.children().isEmpty()){
+                for (Component component: message.children()) {
+                    messageWithoutStyle = messageWithoutStyle.append(removeAllStyleAndColor(component));
+                }
+            } else {
+                messageWithoutStyle = messageWithoutStyle.append(removeAllStyleAndColor(message));
+            }
+            message = messageWithoutStyle;
         }
 
         String[] formatSplit = format.split("\\{message\\}");
@@ -25,5 +39,20 @@ public class Utils {
                         .replace("{suffix}", aurionChatPlayer.getPlayer().getSuffix())
                         .replace("{display_name}", aurionChatPlayer.getPlayer().getDisplayName())
         );
+    }
+
+    private static Component removeAllStyleAndColor(Component component){
+        return component
+                .style(Style.empty())
+                .decorations(
+                        new HashSet<>(Arrays.asList(
+                                TextDecoration.BOLD,
+                                TextDecoration.ITALIC,
+                                TextDecoration.OBFUSCATED,
+                                TextDecoration.STRIKETHROUGH,
+                                TextDecoration.UNDERLINED)
+                        ),
+                        false
+                );
     }
 }
